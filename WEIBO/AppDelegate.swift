74cf3367ee8733a97348e4a275f29ddb36336763
Ferrymen <ferrymen@Ferrymens-Mac.local>
2019-03-12 10:16:12 -0700

    add Home tab

--- WEIBO/AppDelegate.swift
+++ WEIBO/AppDelegate.swift
@@ -17,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // 创建人window
+        // 创建全局颜色
+        UITabBar.appearance().tintColor = UIColor.orange
+        
+        // 创建window
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
--- WEIBO/Classes/Main/MainViewController.swift
+++ WEIBO/Classes/Main/MainViewController.swift
@@ -12,8 +12,24 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        tabBar.tintColor = UIColor.orange
 
         // Do any additional setup after loading the view.
+        
+        // 1､创建子控制器
+        let childVc = HomeViewController()
+        
+        // 2､设置子控制器的属性
+        childVc.title = "首页"
+        childVc.tabBarItem.image = UIImage(named: "tabbar_home")
+        childVc.tabBarItem.selectedImage = UIImage(named: "tabbar_home_highlighted")
+        
+        // 3､包装导航栏控制器
+        let childNav = UINavigationController(rootViewController: childVc)
+        
+        // 4､添加控制器
+        addChild(childNav)
     }
     
 
